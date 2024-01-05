f2c37a84b0cefde16811dd31fd1842fb54b1a7dc
Daiwery <andrey.luk10@gmail.com>
2024-01-05 20:22:46 +0300

    svelte is added

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,12 @@ provider "yandex" {
   zone      = var.ZONE
 }
 
+output "access_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+}
+
+output "secret_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  sensitive = true
+}
+
