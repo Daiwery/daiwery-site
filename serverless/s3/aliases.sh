4b9588e19b4cf8ac7cb7dc7820f1bf26bf80347f
Daiwery <andrey.luk10@gmail.com>
2024-01-11 22:29:16 +0300

    PWD -> SCRIPT_DIR

--- serverless/s3/aliases.sh
+++ serverless/s3/aliases.sh
@@ -1,5 +1,6 @@
-alias list_buckets="python3 $PWD/list_buckets.py"
-alias list_objects="python3 $PWD/list_objects.py"
-alias download="python3 $PWD/download.py"
-alias sync="python3 $PWD/sync.py"
-alias upload_file="python3 $PWD/upload_file.py"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+alias list_buckets="python3 $SCRIPT_DIR/list_buckets.py"
+alias list_objects="python3 $SCRIPT_DIR/list_objects.py"
+alias download="python3 $SCRIPT_DIR/download.py"
+alias sync="python3 $SCRIPT_DIR/sync.py"
+alias upload_file="python3 $SCRIPT_DIR/upload_file.py"
