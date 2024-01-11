9caa233b8d66414c7b8915f3494c2bdde70971ca
Daiwery <andrey.luk10@gmail.com>
2024-01-11 22:19:17 +0300

    delete mimetype from upload_file

--- serverless/s3/upload_file.py
+++ serverless/s3/upload_file.py
@@ -1,6 +1,5 @@
 from core import *
 import argparse
-import mimetypes
 
 parser = argparse.ArgumentParser()
 parser.add_argument("profile", help="The profile from .aws/credentials")
@@ -15,5 +14,4 @@ args = parser.parse_args()
 s3 = get_s3(args.profile)
 args.bucket = get_bucket(s3, args.bucket)
 
-content_type = mimetypes.guess_type(args.key)[0]
-s3.upload_file(args.path, args.bucket, args.key, ExtraArgs={"ContentType": content_type})
+s3.upload_file(args.path, args.bucket, args.key)
